da500d3f042ea1864a31ee4bb9f975a40aff35d2
Bhumika nagar <nagarbhumika56@gmail.com>
2026-03-28 23:57:02 +0530

    added scheduled payments feature

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,10 +2,12 @@ const express= require("express");
 
 const accountRouter = require("./account")
 const userRouter = require("./user");
+const paymentRouter = require("./payment")
 const router = express.Router();
 
 
 router.use("/user",userRouter);
 router.use("/account",accountRouter);
+router.use("/payment",paymentRouter);
 
 module.exports= router;
