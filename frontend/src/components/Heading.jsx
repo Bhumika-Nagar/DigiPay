5e8f3b031a4c3473c84eb027340249add21fec35
Bhumika nagar <nagarbhumika56@gmail.com>
2026-04-03 02:35:45 +0530

    restore components folder

---
+++ frontend/src/components/Heading.jsx
@@ -0,0 +1,7 @@
+export function Heading({ label }) {
+  return (
+    <h1 className="text-3xl font-bold text-white tracking-tight">
+      {label}
+    </h1>
+  );
+}
---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,11 @@
+import { Navigate } from "react-router-dom";
+
+export default function Home() {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    return <Navigate to="/dashboard" />;
+  } else {
+    return <Navigate to="/signin" />;
+  }
+}
